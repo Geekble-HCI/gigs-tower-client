6dcaf6ccff049c2b51d856c720940b5720d77a3b
minbgames <sherlotics@gmail.com>
2025-04-04 20:41:24 +0900

    Add Waiting

--- Module/game_state.py
+++ Module/game_state.py
@@ -39,3 +39,11 @@ class GameStateManager:
     def show_score(self, score):
         self.current_state = GameState.SCORE
         self.screen_update_callback(f"당신의 점수는?\n\n{score}\n\n태그를 하여\n점수를 획득하세요!")
+
+    def show_waiting_screen(self):
+        """게임 상태를 대기 상태로 초기화"""
+        self.current_state = GameState.WAITING
+        self.countdown = 10
+        if self.timer_thread and self.timer_thread.is_alive():
+            self.timer_thread.join(0)
+        self.timer_thread = None
