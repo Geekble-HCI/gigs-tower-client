806f97c3db95abde8f6ac548016b648f66ced563
maker-kiku <siook1239@gmail.com>
2025-09-12 00:48:08 +0900

    arduino rfid code update (PN532 power control)

--- ESP32/rfid-start-handler-arduino/rfid-start-handler-arduino.ino
+++ ESP32/rfid-start-handler-arduino/rfid-start-handler-arduino.ino
@@ -1,30 +1,26 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
-#include <avr/wdt.h> // Watchdog Timer 사용
 
-// SPI 핀 정의
+// SPI 핀 정의 (케이블 색상 표시)
 #define PN532_SCK   2 // orange
 #define PN532_MISO  5 // yellow
 #define PN532_MOSI  3 // green
 #define PN532_SS    4 // blue
+#define PN532_PWR   7 // red, VCC 제어용 핀
+
+#define DELAY_TIME 3000
 
 Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
 
 void setup() {
     Serial.begin(115200);
-    while (!Serial); // 시리얼 준비 대기
+    while (!Serial);
 
-    nfc.begin();
+    pinMode(PN532_PWR, OUTPUT);
+    digitalWrite(PN532_PWR, HIGH); // NFC 전원 ON
 
-    uint32_t versiondata = nfc.getFirmwareVersion();
-    if (!versiondata) {
-        Serial.println("PN532 not found");
-        while (1); // 장치 없음 시 무한 루프
-    }
-
-    nfc.setPassiveActivationRetries(0xFF); // 카드 감지 시도 횟수
-    Serial.println("Ready to read NFC tag...");
+    initNFC();
 }
 
 void loop() {
@@ -36,16 +32,32 @@ void loop() {
     success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
     if (success) {
         // UID 출력
-        Serial.print("UID: ");
         for (uint8_t i = 0; i < uidLength; i++) {
             if (uid[i] < 0x10) Serial.print("0");
             Serial.print(uid[i], HEX);
         }
         Serial.println();
 
-        // 바로 리부팅
-        Serial.println("Restarting board...");
-        wdt_enable(WDTO_15MS); // 15ms 후 강제 리셋
-        while (1); // WDT가 트리거될 때까지 대기
+        // 카드 인식 즉시 전원 차단
+        digitalWrite(PN532_PWR, LOW);
+
+        // 카드 중복 태그 방지 3초 대기
+        delay(DELAY_TIME); 
+
+        // NFC 전원 ON 후 재초기화
+        digitalWrite(PN532_PWR, HIGH);
+        delay(50); // 모듈 안정화
+        initNFC();
     }
+}
+
+// NFC 초기화 함수
+void initNFC() {
+    nfc.begin();
+    uint32_t versiondata = nfc.getFirmwareVersion();
+    if (!versiondata) {
+        Serial.println("PN532 not found");
+        while (1);
+    }
+    nfc.setPassiveActivationRetries(0xFF);
 }
